cfb01f8dfa59185c9d80e841443600c14c2cd9e1
Ben Gelens <ben@bgelens.nl>
2015-12-24 20:28:13 +0100

    Added initial Azure Automation support

---
+++ Classes/AutoLoad/Blue.AutomationAccount.cs
@@ -0,0 +1,14 @@
+namespace Blue
+{
+    public class AutomationAccount
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string id { get; set; }
+        public string ResourceGroupName { get; set; }
+        public string dscMetaConfiguration { get; set; }
+        public string endpoint { get; set; }
+        public string PrimaryKey { get; set; }
+        public string SecondaryKey { get; set; }
+    }
+}
