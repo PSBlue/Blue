d19fb4cc848fbce2ecc70922f15aa0b9984d08ad
Trond Hindenes <trond@hindenes.com>
2016-05-21 21:56:08 +0200

    fixing tests

--- Tests/envinfo.ps1
+++ Tests/envinfo.ps1
@@ -2,17 +2,23 @@ write-output "PS version is: $($PSVersionTable.PSVersion.tostring())"
 Write-Output "OS version is: $(Get-WmiObject win32_operatingsystem | select -ExpandProperty version)"
 write-output "Here is: $(get-location | select -ExpandProperty path)"
 
-$here = get-location | select -ExpandProperty path
+#$here = get-location | select -ExpandProperty path
 $ModuleFolderHere = get-location | select -ExpandProperty path
-$here = $ModuleFolderHere
+#$here = $ModuleFolderHere
 $ModuleFolder = Split-Path $moduleFolderHere -Parent
 
+<#
 $VerbosePreference = Continue
 if (Get-item "$ModuleFolder\LocalVars.Config" -ErrorAction SilentlyContinue)
 {
     . $ModuleFolder\Tests\ConfigureTestEnvironment.ps1 -FilePath $ModuleFolder\LocalVars.config
 }
 $VerbosePreference = SilentlyContinue
+#>
 
-write-outout "Env things:"
+write-output "Env things:"
 write-output "env:logonaccountusername: $($env:logonaccountusername)"
+
+#Always exit with code 0, this script is not important
+$host.SetShouldExit(0)
+exit 
